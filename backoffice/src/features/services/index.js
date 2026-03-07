ee0c4e81abeeb1cf05d885db5ad1a960f66fbcc2
sdjibril <samuel.tilla@rhopenlabs.africa>
2026-03-07 16:23:44 +0100

    chore: remove backend, backoffice and build artifacts

--- backoffice/src/features/services/index.js
+++
@@ -1,3 +0,0 @@
-export { default as ServicesList } from './ServicesList'
-export { default as ServiceRow } from './ServiceRow'
-export { default as ServiceForm } from './ServiceForm'
--- backoffice/src/features/team/index.js
+++
@@ -1,3 +0,0 @@
-export { default as TeamList } from './TeamList'
-export { default as TeamRow } from './TeamRow'
-export { default as TeamForm } from './TeamForm'
